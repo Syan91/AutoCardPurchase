00924dd8fbdef2a2e6bb51904901324ededeef85
syan91 <syan199165@gmail.com>
2020-11-29 10:42:42 +0300

    Test modified

--- src/test/java/ru/netology/selenide/CardPurchaseTest.java
+++ src/test/java/ru/netology/selenide/CardPurchaseTest.java
@@ -14,8 +14,8 @@ import static com.codeborne.selenide.Selenide.open;
   void ShouldPurchaseCard(){
    open("http://localhost:9999");
    SelenideElement form = $ ("[action]");
-   form.$("[data-test-id = name] input" ).setValue("Иван");
-   form.$("[data-test-id= phone] input").setValue("+89284322222");
+   form.$("[data-test-id = name] input" ).setValue("Иван Иванов");
+   form.$("[data-test-id= phone] input").setValue("+79284322222");
    form.$("[data-test-id = agreement]").click();
    form.$("[type= button").click();
    $("[data-test-id = order-success").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
